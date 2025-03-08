cf4feaee8a1b4618205e88f1b3ea51698407504b
cfrancine <cfrancinelara@gmail.com>
2025-03-08 23:41:19 +0800

    Add files via upload

---
+++ Lab 4.py
@@ -0,0 +1,16 @@
+grade = int(input("Grades?:"))
+if grade >=101:
+	print("Invalid Score")
+elif grade >=90:
+	print("Grade A")
+elif grade >=80:
+	print("Grade B")
+elif grade >=70:
+	print("Grade C")
+elif grade >=60:
+	print("Grade D")
+elif grade >=0:
+	print("Grade F")
+	
+else:
+	print("Invalid Score")
